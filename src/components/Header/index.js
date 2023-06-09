05f34d4c1aa2b797abb8d2a3030719386de8e671
HafiRuksana <shafijaruksana78@gmail.com>
2023-06-09 14:17:32 +0000

    solution

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+/* Write your CSS here */
+import './index.css'
+
+const Header = () => (
+  <div className="header-container">
+    <h1 className="header-text">Header</h1>
+  </div>
+)
+
+export default Header
